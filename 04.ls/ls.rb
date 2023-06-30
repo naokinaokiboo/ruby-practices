ca2f989d141da8138daa2d423100219cd61bab52
naokinaokiboo <naokinaokiboo@gmail.com>
2023-07-01 07:47:26 +0900

    リファクタリング
    ・トップレベルのインスタンス変数を定数に変更
    ・改行を print "\n" から puts に変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-@max_columns = 3
-@terminal_width = `tput cols`.to_i
+MAX_COLUMNS = 3
+TERMINAL_WIDTH = `tput cols`.to_i
 
 # 半角英数字以外のファイル名でも表示が崩れないための対応
 def ljust_for_mbchar(str, width)
@@ -15,7 +15,7 @@ def display(files)
 
   max_bytes_filename = files.max_by(&:bytesize).bytesize
 
-  num_of_columns = @max_columns.downto(1).find { |n| max_bytes_filename * n + (n - 1) <= @terminal_width }
+  num_of_columns = MAX_COLUMNS.downto(1).find { |n| max_bytes_filename * n + (n - 1) <= TERMINAL_WIDTH }
   num_of_rows = (files.size / num_of_columns.to_f).ceil
 
   matrix = files.each_slice(num_of_rows).map { |subset_files| subset_files }
@@ -25,7 +25,7 @@ def display(files)
     subset_files.each do |file|
       print ljust_for_mbchar(file, max_bytes_filename + 1) unless file.nil?
     end
-    print "\n"
+    puts
   end
 end
 
