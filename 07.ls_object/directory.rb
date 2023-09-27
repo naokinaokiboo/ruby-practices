cebf6fc8f084d9f1319eac4f90cba51909c57454
naokinaokiboo <naokinaokiboo@gmail.com>
2023-09-27 23:55:58 +0900

    Directoryクラスにpathの読み取りメソッドを追加

--- 07.ls_object/directory.rb
+++ 07.ls_object/directory.rb
@@ -3,14 +3,14 @@
 require_relative 'entry'
 
 class Directory
-  attr_reader :entries
+  attr_reader :entries, :path
 
   def initialize(directory_path)
     @path = directory_path
   end
 
   def generate_list_content(opt_param)
-    @entries = Dir.entries(@path).map { |entry| Entry.new([@path, entry].join('/'), entry) }
+    @entries = Dir.entries(path).map { |entry| Entry.new([path, entry].join('/'), entry) }
     FormatterFactory.create(opt_param).generate_formatted_content(self, opt_param)
   end
 end
