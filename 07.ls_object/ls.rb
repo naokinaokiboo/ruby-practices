0b5b455ee385abcaa4c342e6e0514e6def2fd155
naokinaokiboo <naokinaokiboo@gmail.com>
2023-09-28 13:27:33 +0900

    main関数で行っていた処理をLSクラスに変更

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -7,36 +7,41 @@ require_relative 'unrelated_file_container'
 require_relative 'directory'
 require_relative 'formatter_factory'
 
-def main
-  opt_param = OptionalParameter.new
-
-  paths = ARGV.empty? ? [Dir.pwd] : ARGV.sort
-  existent_paths, nonexistent_paths = paths.partition { |path| File.exist?(path) }
-  directory_paths, file_paths = existent_paths.partition { |path| File.ftype(path) == 'directory' }
-
-  sorted_file_paths = sort_with_reverse_option(file_paths, opt_param.sort_reverse?)
-  sorted_directory_paths = sort_with_reverse_option(directory_paths, opt_param.sort_reverse?)
-
-  containers = []
-  containers << NonExistentContainer.new(nonexistent_paths) if nonexistent_paths.any?
-  containers << UnrelatedFileContainer.new(sorted_file_paths) if sorted_file_paths.any?
-  sorted_directory_paths.each do |directory_path|
-    containers << Directory.new(directory_path)
+class LS
+  def initialize
+    @opt_param = OptionalParameter.new
+    @paths = ARGV.empty? ? [Dir.pwd] : ARGV.sort
   end
 
-  result =
-    containers.each_with_object([]).with_index do |(container, result), index|
+  def generate_content
+    generate_containers.each_with_object([]) do |container, result|
       container.generate_entries(opt_param)
-      result << "#{container.path}:" if container.instance_of?(Directory)
+      result << "\n#{container.path}:" if container.instance_of?(Directory)
       result << FormatterFactory.create(container, opt_param).generate_formatted_content
-      result << "\n" if !container.instance_of?(NonExistentContainer) && index != containers.size - 1
+    end.join("\n")
+  end
+
+  private
+
+  attr_reader :opt_param, :paths
+
+  def sort_with_reverse_option(paths, reverse)
+    reverse ? paths.sort.reverse : paths.sort
+  end
+
+  def generate_containers
+    existent_paths, nonexistent_paths = paths.partition { |path| File.exist?(path) }
+    directory_paths, file_paths = existent_paths.partition { |path| File.ftype(path) == 'directory' }
+    sorted_file_paths = sort_with_reverse_option(file_paths, opt_param.sort_reverse?)
+    sorted_directory_paths = sort_with_reverse_option(directory_paths, opt_param.sort_reverse?)
+
+    containers = []
+    containers << NonExistentContainer.new(nonexistent_paths) if nonexistent_paths.any?
+    containers << UnrelatedFileContainer.new(sorted_file_paths) if sorted_file_paths.any?
+    sorted_directory_paths.each_with_object(containers) do |directory_path, result|
+      result << Directory.new(directory_path)
     end
-
-  puts result
+  end
 end
 
-def sort_with_reverse_option(paths, reverse)
-  reverse ? paths.sort.reverse : paths.sort
-end
-
-main
+puts LS.new().generate_content
