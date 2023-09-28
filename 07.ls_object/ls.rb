2cb35c602c1bb88754e2baf80ccaeaffdd115cad
naokinaokiboo <naokinaokiboo@gmail.com>
2023-09-28 11:24:03 +0900

    ls実行main関数を実装

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,20 +1,40 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'optparse'
+require_relative 'optional_parameter'
+require_relative 'non_existent_container'
+require_relative 'unrelated_file_container'
+require_relative 'directory'
 
 def main
-  opt_params = ARGV.getopts('arl')
-  targets = ARGV.empty? ? [Dir.pwd] : ARGV.sort
-  existent_targets, noexistent_targets = targets.partition { |target| File.exist?(target) }
-  directories, files = existent_targets.partition { |target| File.ftype(target) == 'directory' }
+  opt_param = OptionalParameter.new
 
-  # TODO
-  # 各Pathクラスから文字列作成(形式はFormatterクラスの責務とする)
-  # 取得した文字列を標準出力へ表示
-  #  -> 表示する順番については、こちらの責務
-  #  -> 引数を複数指定した場合のディレクトリ名表示もこちらの責務とする
-  #     (表示する対象が複数あることを、Formatterは知らない)
+  paths = ARGV.empty? ? [Dir.pwd] : ARGV.sort
+  existent_paths, nonexistent_paths = paths.partition { |path| File.exist?(path) }
+  directory_paths, file_paths = existent_paths.partition { |path| File.ftype(path) == 'directory' }
+
+  sorted_file_paths = sort_with_reverse_option(file_paths, opt_param.sort_reverse?)
+  sorted_directory_paths = sort_with_reverse_option(directory_paths, opt_param.sort_reverse?)
+
+  containers = []
+  containers << NonExistentContainer.new(nonexistent_paths) if nonexistent_paths.any?
+  containers << UnrelatedFileContainer.new(sorted_file_paths) if sorted_file_paths.any?
+  sorted_directory_paths.each do |directory_path|
+    containers << Directory.new(directory_path)
+  end
+
+  result =
+    containers.each_with_object([]).with_index do |(container, result), index|
+      result << "#{container.path}:" if container.instance_of?(Directory)
+      result << container.generate_list_content(opt_param)
+      result << "\n" if !container.instance_of?(NonExistentContainer) && index != containers.size - 1
+    end
+
+  puts result
+end
+
+def sort_with_reverse_option(items, reverse)
+  reverse ? items.sort.reverse : items.sort
 end
 
 main
